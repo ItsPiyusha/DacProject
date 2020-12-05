a1539f46ec4b7bac1f8aaafc05e358c4d588d77e
Piyusha Pawar <32691430+ItsPiyusha@users.noreply.github.com>
2020-12-05 13:12:43 +0530

    added tailwind css and index.html
    @RachanaD1997

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors:{
+        blue:{
+          100: rgb(8, 15, 76),
+          200: rgb(24, 29, 171),
+          300: rgb(24, 142, 233),
+          400: '#188EE9'
+        }
+    },
+    extend: {
+     
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
