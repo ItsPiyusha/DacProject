b10889e1534c136f74a53b9aa48ba68a3f6116d4
Piyusha Pawar <32691430+ItsPiyusha@users.noreply.github.com>
2020-12-05 20:24:20 +0530

    added navbar partially

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,11 +4,9 @@ module.exports = {
   theme: {
     colors:{
         blue:{
-          100: rgb(8, 15, 76),
-          200: rgb(24, 29, 171),
-          300: rgb(24, 142, 233),
+          
           400: '#188EE9',
-          500:  rgb(55, 144, 247)
+         
         }
     },
     extend: {
