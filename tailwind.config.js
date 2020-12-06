42efa533338a9e5517e4a2b0010e915580a705da
Piyusha Pawar <32691430+ItsPiyusha@users.noreply.github.com>
2020-12-06 11:01:26 +0530

    navbar updated
    @RachanaD1997

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
+  fontFamily: {
+    gotham: ['Gotham Pro','sans-serif']
+  },
+  screens: {
+    sm: '640px',
+    md: '768px',
+    lg:'1024px',
+    xl:'1280px',
+    xxl:'1590px'
+  },
   theme: {
     colors:{
         blue:{
-          
-          400: '#188EE9',
+          100: '#489EE9',
+          200: '#20438A',
+          400: '#188EE9'
          
         }
     },
